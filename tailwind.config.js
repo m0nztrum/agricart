33cbb86313f7ca529c71c4b8392467a0b1430808
m0nztrum <delafiado@gmail.com>
2024-11-23 23:40:32 +0000

    color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                apple: {
+                    50: '#f0fdf2',
+                    100: '#dcfce2',
+                    200: '#bbf7c7',
+                    300: '#8ff0a4',
+                    400: '#4ade6a',
+                    500: '#22c546',
+                    600: '#16a335',
+                    700: '#15802d',
+                    800: '#166529',
+                    900: '#145324',
+                    950: '#052e10',
+                },
+            },
+        },
     },
     plugins: [],
 };
