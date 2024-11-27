5cde678f39eb7d921cef6ca4b52f2d03460e9034
m0nztrum <delafiado@gmail.com>
2024-11-27 20:23:20 +0000

    style: homepage

---
+++ src/components/home/Testimonials.tsx
@@ -0,0 +1,29 @@
+import { Star } from 'lucide-react';
+
+export const Testimonials = () => {
+    return (
+        <div className="px-16 py-28">
+            <div className="grid grid-cols-2 gap-20">
+                <div>
+                    <img src="/images/placeholder-image-3610342416.png" alt="testimonial" className="rounded-lg" />
+                </div>
+                <div>
+                    <div className="flex space-x-1">
+                        {[...Array(5)].map((_, i) => (
+                            <Star key={i} />
+                        ))}
+                    </div>
+                    <p className="my-8 text-2xl font-medium leading-relaxed">
+                        "The quality of the farm products is exceptional! I couldn't be happier with my purchase."
+                    </p>
+
+                    {/* Author Info */}
+                    <div>
+                        <p>John Doe</p>
+                        <p>Farm Manager, Green Acres. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { Hero } from '../components/home/Hero';
 import { Services } from '../components/home/Services';
 import { ProductPreview } from '../components/home/ProductPreview';
+import { Testimonials } from '../components/home/Testimonials';
+import { Cta } from '../components/home/Cta';
 
 export const Home = () => {
     return (
@@ -8,6 +10,8 @@ export const Home = () => {
             <Hero />
             <Services />
             <ProductPreview />
+            <Testimonials />
+            <Cta />
         </div>
     );
 };
