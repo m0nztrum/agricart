157f55fd914561a72ba92ceae134c7a19928dbb8
m0nztrum <delafiado@gmail.com>
2024-12-30 20:59:11 +0000

    removed cta section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { Hero } from '../components/home/Hero';
 import { Services } from '../components/home/Services';
 import { ProductPreview } from '../components/home/ProductPreview';
 import { Testimonials } from '../components/home/Testimonials';
-import { Cta } from '../components/home/Cta';
 
 export const Home = () => {
     return (
